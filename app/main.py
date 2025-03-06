78b49587286bf51d99b1bae57f910bb19d018291
jungohkr <132813490+jungohkr@users.noreply.github.com>
2025-03-06 21:12:54 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
-from models import Problem, engine
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
