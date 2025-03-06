282d099c9c323a9295b31eb8a4105aa9af4e7b32
jungohkr <132813490+jungohkr@users.noreply.github.com>
2025-03-06 21:14:12 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
